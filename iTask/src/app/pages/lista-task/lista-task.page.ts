7c01b70889d2216c75962d6cb2a23e569999811a
Andrea <andripulc@hotmail.it>
2020-07-06 17:35:02 +0200

    commit listaT

--- iTask/src/app/pages/lista-task/lista-task.page.ts
+++ iTask/src/app/pages/lista-task/lista-task.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {IonReorderGroup} from '@ionic/angular';
 
 @Component({
   selector: 'app-lista-task',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-task.page.scss'],
 })
 export class ListaTaskPage implements OnInit {
+  @ViewChild(IonReorderGroup) reorderGroup: IonReorderGroup;
 
   constructor() { }
+  doReorder(ev: any) {
+    // The `from` and `to` properties contain the index of the item
+    // when the drag started and ended, respectively
+    console.log('Dragged from index', ev.detail.from, 'to', ev.detail.to);
+
+    // Finish the reorder and position the item in the DOM based on
+    // where the gesture ended. This method can also be called directly
+    // by the reorder group
+    ev.detail.complete();
+  }
+
+  toggleReorderGroup() {
+    this.reorderGroup.disabled = !this.reorderGroup.disabled;
+  }
+
 
   ngOnInit() {
+  }
+  dettagli_task(){
+    console.log('ciao');
   }
 
 }
