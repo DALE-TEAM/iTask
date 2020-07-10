9e40d2c41d1ce3ac8457e4af339a99f24329059d
Andrea <andripulc@hotmail.it>
2020-07-10 19:04:16 +0200

    commit add reminder2

--- iTask/src/app/pages/add-reminder/add-reminder.page.ts
+++ iTask/src/app/pages/add-reminder/add-reminder.page.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 
 @Component({
@@ -7,16 +8,50 @@ import {Component, OnInit, ViewChild} from '@angular/core';
   styleUrls: ['./add-reminder.page.scss'],
 })
 export class AddReminderPage implements OnInit {
+  icon: any;
+  color:any;
+
   constructor() { }
 
   ngOnInit() {
+    this.color='lightblue';
+    this.icon='list';
   }
 
   setColor(color : any){
+    this.color=color;
+
     console.log(color);
   }
-  setIcon(name : any){
-    console.log(name);
+  setIcon(icon : any){
+    this.icon=icon;
+
+    console.log(icon);
+  }
+
+  form_addReminder = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+    color: new FormControl('', [
+      Validators.required,
+      Validators.minLength(1),
+    ]),
+    icon: new FormControl('', [
+      Validators.required,
+      Validators.minLength(1),
+    ]),
+  });
+
+  createReminder(){
+
+    this.form_addReminder.value['icon']=this.icon;
+    this.form_addReminder.value['color']=this.color;
+    console.log(this.form_addReminder.value['name']);
+    console.log(this.form_addReminder.value['color']);
+    console.log(this.form_addReminder.value['icon']);
+
   }
 
 }
