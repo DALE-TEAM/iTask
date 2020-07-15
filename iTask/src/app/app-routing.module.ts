dc9a5003a65e69d2fae8bb96614dd0bd88b727ae
Lorenc <lorenc.markja@student.univaq.it>
2020-07-15 18:58:29 +0200

    bug fixing

--- iTask/src/app/app-routing.module.ts
+++ iTask/src/app/app-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
   {
     path: 'dashboard',
     loadChildren: () => import('./pages/dashboard/dashboard.module').then( m => m.DashboardPageModule),
-    //canActivateChild: [AuthGuard]
+    canActivateChild: [AuthGuard]
   },
   {
     path: 'registration',
